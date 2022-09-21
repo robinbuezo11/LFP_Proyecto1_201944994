9e16a8a55e4fb22e3e4c62e63a2f83a5175359aa
unknown <3384471212007@ingenieria.usac.edu.gt>
2022-09-21 17:59:55 -0600

    [add] status

--- DFA.py
+++ DFA.py
@@ -1,9 +1,50 @@
 
 class DFA:
-    def __init__(self,text) -> None:
+    def __init__(self,text=[]) -> None:
         self.__text = text
+        self.__status = 0
 
-    def analyze(self):
+    def analyze(self, text=[]):
+        if text == []:
+            text = self.__text
+        errors =[]
+        row = 1
+        column = 1
+
+        for line in text:
+            column = 1
+            self.__status = 0
+            for char in line:
+                if self.__status == 0:
+                    self.__status0(char=char)
+                elif self.__status == 1:
+                    self.__status1(char=char)
+                elif self.__status == 2:
+                    self.__status2(char=char)
+                elif self.__status == 3:
+                    self.__status3(char=char)
+                elif self.__status == 4:
+                    self.__status4(char=char)
+                elif self.__status == 5:
+                    self.__status5(char=char)
+                elif self.__status == 6:
+                    self.__status6(char=char)
+                elif self.__status == 7:
+                    self.__status7(char=char)
+                elif self.__status == 8:
+                    self.__status8(char=char)
+                elif self.__status == 9:
+                    self.__status9(char=char)
+                elif self.__status == 10:
+                    self.__status10(char=char)
+                elif self.__status == 11:
+                    self.__status11(char=char)
+                elif self.__status == 12:
+                    self.__status12(char=char)    
+                column+=1
+            row +=1
+            
+    def __status0(self, char):
         pass
 
     def __status1(self, char):
@@ -42,3 +83,8 @@ class DFA:
     def __status12(self, char):
         pass
 
+    def getText(self):
+        return self.__text
+
+    def setText(self, text):
+        self.__text = text
--- ManagerFile.py
+++ ManagerFile.py
@@ -4,27 +4,25 @@ from DFA import DFA
 
 class ManagerFile:
     def __init__(self) -> None:
-        self.__file = None
-        self.__filelines = None
+        self.__data = []
 
     #----------------------- Functions ----------------------------
     def openFile(self,ruta):   #Metodo para leer el archivo
         try:
-            self.__file = open(ruta,'r', encoding='utf-8')
-            self.__filelines = self.__file.readlines()
+            file = open(ruta,'w+', encoding='utf-8')
+            self.__data = file.readlines()
             
             msgbx.showinfo('Archivo Cargado','El archivo se cargó exitosamente')
             
-            self.__readFile(self.__filelines)
+            self.__readFile(self.__data)
         except Exception:
             msgbx.showerror("ERROR",'Error en la carga, revise que los datos y la ruta de su archivo sean correctos.')
-            if self.__file is not None:
-                self.__file.close()
+            if file is not None:
+                file.close()
 
-
-    def __readFile(self, file):     #Metodo para analizar el archivo
-        dfa = DFA(file)
-        dfa.analyze()
+    def __readFile(self, filelines=[]):     #Metodo para analizar el archivo
+        dfa = DFA()
+        dfa.analyze(filelines)
 
     def getData(self):
         return self.__data
