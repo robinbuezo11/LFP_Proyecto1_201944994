32469f703b69938fa3c039ab9220c22ae9faccf3
robinbuezo11 <3384471212007@ingenieria.usac.edu.gt>
2022-09-25 03:04:13 -0600

    [add] abrir, guardar, guardar como

--- ManagerFile.py
+++ ManagerFile.py
@@ -1,32 +1,56 @@
 from tkinter import messagebox as msgbx
+from tkinter.filedialog import askopenfilename, asksaveasfile
 
 from DFA import DFA
 
 class ManagerFile:
     def __init__(self) -> None:
+        self.__path = None
         self.__data = []
 
     #----------------------- Functions ----------------------------
-    def openFile(self,ruta):   #Metodo para leer el archivo
+    def openFile(self):   #Metodo para leer el archivo
         try:
-            file = open(ruta,'w+', encoding='utf-8')
-            self.__data = file.readlines()
-            
+            path = askopenfilename()
+            file = open(path,'r', encoding='utf-8')
+            self.__path = path
+            if file is not None:
+                self.__data = file.readlines()
+                file.close()
             msgbx.showinfo('Archivo Cargado','El archivo se cargó exitosamente')
-            
-            self.__readFile(self.__data)
         except Exception:
-            msgbx.showerror("ERROR",'Error en la carga, revise que los datos y la ruta de su archivo sean correctos.')
             if file is not None:
                 file.close()
+            msgbx.showerror("ERROR",'Error en la carga, revise que los datos y la ruta de su archivo sean correctos.')
 
-    def __readFile(self, filelines=[]):     #Metodo para analizar el archivo
+    def analyzeText(self, filelines=[]):     #Metodo para analizar el archivo
         dfa = DFA()
         dfa.analyze(filelines)
+
+    def save(self,data,saveas=False):
+        try:
+            if saveas:
+                file = asksaveasfile(title='Guardar como',defaultextension='.txt', filetypes=[('Todos','*.*')],mode='w')
+            else:
+                file = open(self.__path,'w', encoding='utf-8')
+            file.write(data)
+            file.close()
+            msgbx.showinfo('Guardado','Archivo guardado exitosamente')
+        except Exception as e:
+            if file is not None:
+                file.close()
+            msgbx.showerror("ERROR",e)
 
     def getData(self):
         return self.__data
 
     def setData(self, data):
         self.__data = data
+
+    def getPath(self):
+        return self.__path
+
+    def setPath(self, path):
+        self.__path = path
+
 
--- WindowMain.py
+++ WindowMain.py
@@ -1,38 +1,65 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox as msgbx
+from tkinter.filedialog import askopenfilename, asksaveasfile
+from ManagerFile import ManagerFile
 
 class WindowMain(ttk.Frame):
-    def __init__(self, master) -> None:
+    def __init__(self, master, mfile=ManagerFile()) -> None:
         super().__init__(master)
         master.title('Proyecto 1')
         master.geometry('800x700')
         master.config(background='sky blue')
         master.resizable(False,False)
+        self.__mfile = mfile
 
-        menu = tk.Menu()
+        self.__menu = tk.Menu()
 
-        menufile = tk.Menu(menu, tearoff=False)
-        menu.add_cascade(menu=menufile, label='Archivo')
+        self.__menufile = tk.Menu(self.__menu, tearoff=False)
+        self.__menu.add_cascade(menu=self.__menufile, label='Archivo')
 
-        menufile.add_command(label='Abrir', command=self.__open)
-        menufile.add_command(label='Guardar', command=self.__open)
-        menufile.add_command(label='Guardar Como', command=self.__open)
-        menufile.add_command(label='Analizar', command=self.__open)
-        menufile.add_command(label='Errores', command=self.__open)
-        menufile.add_command(label='Salir', command=self.__open)
+        self.__menufile.add_command(label='Abrir', command=self.__open)
+        self.__menufile.add_command(label='Guardar', command=self.__save)
+        self.__menufile.add_command(label='Guardar Como', command=self.__saveAs)
+        self.__menufile.add_command(label='Analizar', command=self.__open)
+        self.__menufile.add_command(label='Errores', command=self.__open)
+        self.__menufile.add_command(label='Salir', command=self.__exit)
 
-        menuhelp = tk.Menu(menu, tearoff=False)
-        menu.add_cascade(menu=menuhelp, label='Ayuda')
+        self.__menuhelp = tk.Menu(self.__menu, tearoff=False)
+        self.__menu.add_cascade(menu=self.__menuhelp, label='Ayuda')
 
-        menuhelp.add_command(label='Manual Técnico', command=self.__open)
-        menuhelp.add_command(label='Manual de Usuario', command=self.__open)
-        menuhelp.add_command(label='Temas de Ayuda', command=self.__open)
+        self.__menuhelp.add_command(label='Manual Técnico', command=self.__open)
+        self.__menuhelp.add_command(label='Manual de Usuario', command=self.__open)
+        self.__menuhelp.add_command(label='Temas de Ayuda', command=self.__open)
 
-        master.config(menu=menu)
+        master.config(menu=self.__menu)
 
-        entrytext = ttk.Entry(master=master)
-        entrytext.place(x=20, y=20, width=760, height=660)
+        self.__entrytext = tk.Text(master=master)
+        self.__entrytext.place(x=20, y=20, width=760, height=660)
 
     def __open(self):
-        msgbx.showinfo('Info','Has presionado el boton de abrir')
+        try:
+            self.__mfile.openFile()
+            self.__entrytext.delete(1.0,'end')
+            for line in self.__mfile.getData():
+                self.__entrytext.insert('end', line)
+        except Exception as e:
+            print(msgbx.showerror('Error',e))
+
+    def __save(self):
+        try:
+            self.__mfile.save(self.__entrytext.get(1.0,'end'))
+        except Exception as e:
+            print(msgbx.showerror('Error',e))
+
+    def __saveAs(self):
+        try:
+            self.__mfile.save(self.__entrytext.get(1.0,'end'),True)
+            self.__entrytext.delete(1.0,'end')
+            for line in self.__mfile.getData():
+                self.__entrytext.insert('end', line)
+        except Exception as e:
+            print(msgbx.showerror('Error',e))
+
+    def __exit(self):
+        self.master.destroy()
